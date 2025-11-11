c82c7a412e04cc5e7ab4c03132b30a31893e8040
rvls2 <167191194+rvls2@users.noreply.github.com>
2025-11-11 14:17:09 -0300

    Update timer.c

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -92,8 +92,9 @@ timer_sleep (int64_t ticks)
   int64_t start = timer_ticks ();
 
   ASSERT (intr_get_level () == INTR_ON);
-  while (timer_elapsed (start) < ticks) 
-    thread_yield ();
+
+  if (timer_elapsed(start) < ticks) // Substituir o busy wait por um estado de sleep
+    thread_sleep(start + ticks);
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
@@ -172,6 +173,9 @@ timer_interrupt (struct intr_frame *args UNUSED)
 {
   ticks++;
   thread_tick ();
+
+  // Adicionado
+  thread_wakeup(timer_ticks());
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
