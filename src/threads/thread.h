011da3f82f1f7f3aef4f4da205120a94a2083ab2
rvls2 <167191194+rvls2@users.noreply.github.com>
2025-11-11 14:35:39 -0300

    wakeup_time

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -93,6 +93,9 @@ struct thread
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 
+    // Alarm Clock
+    int64_t wakeup_tick;
+
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
@@ -125,6 +128,9 @@ const char *thread_name (void);
 
 void thread_exit (void) NO_RETURN;
 void thread_yield (void);
+bool thread_compare(const struct list_elem *a, const struct list_elem *b, void *aux);
+void thread_sleep (int64_t ticks);
+void thread_wakeup (int64_t current_tick);
 
 /* Performs some operation on thread t, given auxiliary data AUX. */
 typedef void thread_action_func (struct thread *t, void *aux);
